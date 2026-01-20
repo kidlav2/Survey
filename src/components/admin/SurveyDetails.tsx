c7a97d293e2e088c5a4d09f0670bb1bae20f164b
MacBook <MacBook@MacBook-Air.local>
2026-01-20 15:11:40 -0800

    Fix QR code to use absolute URL with protocol and domain

--- src/components/admin/SurveyDetails.tsx
+++ src/components/admin/SurveyDetails.tsx
@@ -141,7 +141,7 @@ export default function SurveyDetails() {
     }
   };
 
-  const surveyLink = `/survey/${id}`;
+  const surveyLink = `${window.location.origin}/survey/${id}`;
 
   const copyToClipboard = () => {
     navigator.clipboard.writeText(surveyLink);
